6e8c2b05e525fe5699c17bfe2289e6ad1dce94cb
Goudurixx <63597122+Goudurixx@users.noreply.github.com>
2025-04-30 13:18:22 +0200

    🎉 : Begin PortFolio
    This is the very first commit initiliasing the KMP project.

---
+++ SylvainRousseau/composeApp/src/commonMain/kotlin/user/goudurixx/portfolio/Greeting.kt
@@ -0,0 +1,9 @@
+package user.goudurixx.portfolio
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ SylvainRousseau/composeApp/src/iosMain/kotlin/user/goudurixx/portfolio/MainViewController.kt
@@ -0,0 +1,5 @@
+package user.goudurixx.portfolio
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
